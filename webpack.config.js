ad36864754749af55ca9d8e4fe80c72c1e251c4a
chris ercilla <chris ercilla>
2017-04-02 21:56:35 +0700

    Please enter the commit message for your changes. Lines starting

--- webpack.config.js
+++ webpack.config.js
@@ -9,26 +9,119 @@ var root = function (args) {
 };
 
 module.exports = (envOptions) => { 
-        envOptions = envOptions || {};
-        const config = {
-            entry: './src/main.ts',
-            output: {
+        envOptions = envOptions || {}
+        const config = {};
+        // const config = {
+        //     entry: './src/main.ts',
+        //     output: {
+        //         path: __dirname + '/dist',
+        //         filename: 'app.bundle.js'
+        //     },
+        //     resolve: {
+        //         extensions: ['.js', '.ts', '.html','.css']
+        //     },
+        //     module: {
+                // loaders: [
+                //     { test: /\.ts$/, loader: '@ngtools/webpack' },
+                //     // {test: /\.ts$/, loader: 'ts-loader!angular2-template-loader'},
+                //     // // {test: /\.ts$/, exclude:/\..component\.ts$/, loader: 'ts-loader'},
+                //     {test: /\.html$/, loader: 'raw-loader'},
+                //     {test: /\.css$/, loader: 'raw-loader'}
+                // ]
+            // }            
+            // plugins: [
+            //    new AotPlugin({
+            //         tsConfigPath: root('./tsconfig.aot.json'),
+            //         entryModule: root('src', 'app', 'app.module#AppModule'),
+            //         mainPath: root('src', 'main.ts'),
+            //         skipCodeGeneration: false
+            //     }),
+            //     new webpack.optimize.UglifyJsPlugin({
+            //         beautify: false,
+            //         mangle: {
+            //             screw_ie8: true,
+            //             keep_fnames: true
+            //         },
+            //         compress: {
+            //             warnings: false,
+            //             screw_ie8: true
+            //         },
+            //         comments: false
+            //     }),                
+            //     new HtmlWebpackPlugin({
+            //         template: './src/index.html'
+            //     }),
+            //     new webpack.DefinePlugin({
+            //         app: {
+            //             environment: JSON.stringify(process.env.APP_ENVIRONMENT || 'development')
+            //         }
+            //     })
+            // ]
+        // }
+        console.log(envOptions.MODE);
+        if (envOptions.MODE === 'prod') {
+            config.entry = './src/main.ts',
+            config.output = {
                 path: __dirname + '/dist',
                 filename: 'app.bundle.js'
-            },
-            module: {
-                loaders: [
+            };
+            config.resolve = {
+                extensions: ['.js', '.ts', '.html','.css']
+            };
+            config.module = {}
+            config.module.loaders = [
                     { test: /\.ts$/, loader: '@ngtools/webpack' },
                     // {test: /\.ts$/, loader: 'ts-loader!angular2-template-loader'},
                     // // {test: /\.ts$/, exclude:/\..component\.ts$/, loader: 'ts-loader'},
                     {test: /\.html$/, loader: 'raw-loader'},
                     {test: /\.css$/, loader: 'raw-loader'}
-                ]
-            },
-            resolve: {
-                extensions: ['.js', '.ts', '.html','.css']
-            },
-            plugins: [
+                ];
+            config.plugins = [
+               new AotPlugin({
+                    tsConfigPath: root('./tsconfig.aot.json'),
+                    entryModule: root('src', 'app', 'app.module#AppModule'),
+                    mainPath: root('src', 'main.ts'),
+                    skipCodeGeneration: false
+                }),
+                new webpack.optimize.UglifyJsPlugin({
+                    beautify: false,
+                    mangle: {
+                        screw_ie8: true,
+                        keep_fnames: true
+                    },
+                    compress: {
+                        warnings: false,
+                        screw_ie8: true
+                    },
+                    comments: false
+                }),                
+                new HtmlWebpackPlugin({
+                    template: './src/index.html'
+                }),
+                new webpack.DefinePlugin({
+                    app: {
+                        environment: JSON.stringify('production')
+                    }
+                })
+            ]     
+        } else {
+            config.entry = './src/main-jit.ts',
+            config.output = {
+                    path: __dirname + '/dist',
+                    filename: 'app.bundle.js'
+                },
+            config.resolve = {
+                    extensions: ['.js', '.ts', '.html','.css']
+            };
+            config.module = {};
+            config.module.loaders = [
+                    { test: /\.ts$/, loader: '@ngtools/webpack' },
+                    // {test: /\.ts$/, loader: 'ts-loader!angular2-template-loader'},
+                    // {test: /\.ts$/, exclude:/\..component\.ts$/, loader: 'ts-loader'},
+                    {test: /\.html$/, loader: 'raw-loader'},
+                    {test: /\.css$/, loader: 'raw-loader'}
+                ];
+            config.plugins = [
                new AotPlugin({
                     tsConfigPath: root('./tsconfig.aot.json'),
                     entryModule: root('src', 'app', 'app.module#AppModule'),
@@ -55,32 +148,8 @@ module.exports = (envOptions) => {
                         environment: JSON.stringify(process.env.APP_ENVIRONMENT || 'development')
                     }
                 })
-            ]
+            ]            
         }
-
-        // if (envOptions.MODE === 'prod') {
-        //     config.module.loaders.push(
-        //         { test: /\.ts$/, loaders: ['@ngtools/webpack'] }
-        //     );
-        //     config.plugins.push(
-        //         new AotPlugin({
-        //             tsConfigPath: './tsconfig.json',
-        //             entryModule: 'src/app/app.module#AppModule'
-        //         })
-        //         // new webpack.optimize.UglifyJsPlugin({
-        //         //     beautify: false,
-        //         //     mangle: {
-        //         //         screw_ie8: true,
-        //         //         keep_fnames: true
-        //         //     },
-        //         //     compress: {
-        //         //         warnings: false,
-        //         //         screw_ie8: true
-        //         //     },
-        //         //     comments: false
-        //         // }),
-        //     );
-        // }
 
         return config
     
